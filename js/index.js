be1ceaf3021b348337cd9e4a08a241376f890c3b
Jonathan <warkentin.jon@gmail.com>
2019-01-29 17:20:29 -0600

    add showNavItems and hideNavItems functions on mouseover and mouseout of name, respectively

--- js/index.js
+++ js/index.js
@@ -1,15 +1,32 @@
-var name = document.getElementById("nav-name");
-var development = document.getElementById("nav-development");
-var video = document.getElementById("nav-video");
-var photo = document.getElementById("nav-photo");
-var contact = document.getElementById("nav-contact");
+let name = document.getElementById("nav-name");
+let development = document.getElementById("nav-development");
+let video = document.getElementById("nav-video");
+let photo = document.getElementById("nav-photo");
+let contact = document.getElementById("nav-contact");
 
 var navChildren = [development, video, photo, photo, contact];
 
+name.addEventListener("mouseover", showNavItems);
+name.addEventListener("mouseout", hideNavItems);
+
+/*
 function showNavItems() {
   for (i = 0; i < 5; i++) {
     navChildren[i].style.display = "block";
   }
 }
+*/
 
-name.addEventListener("mouseenter", showNavItems());
+function showNavItems() {
+  for (i = 0; i < 5; i++) {
+    navChildren[i].style.display = "block";
+  }
+  console.log("showNavItems function triggered");
+}
+
+function hideNavItems() {
+  for (i = 0; i < 5; i++) {
+    navChildren[i].style.display = "none";
+  }
+  console.log("hideNavItems function triggered");
+}
