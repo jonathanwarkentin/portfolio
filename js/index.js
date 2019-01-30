d7769fe9ef6a7df67e7c2ce8580ed977a3d43c30
Jonathan <warkentin.jon@gmail.com>
2019-01-30 16:06:43 -0600

    make current menu appearance script way simpler

--- js/index.js
+++ js/index.js
@@ -3,22 +3,17 @@ let development = document.getElementById("nav-development");
 let video = document.getElementById("nav-video");
 let photo = document.getElementById("nav-photo");
 let contact = document.getElementById("nav-contact");
+let landingNav = document.getElementById("landing-nav");
 
 var navChildren = [development, video, photo, contact];
-
-var mousePresence = false;
 
 name.addEventListener("mouseover", showNavItems);
 
 function showNavItems() {
-  mousePresence = true;
   for (i = 0; i < 4; i++) {
     navChildren[i].style.display = "block";
-    navChildren[i].addEventListener("mouseout", canHideNav);
-    navChildren[i].addEventListener("mouseover", refreshShowNav);
   }
-  name.addEventListener("mouseout", canHideNav);
-  name.addEventListener("mouseover", refreshShowNav);
+  landingNav.addEventListener("mouseleave", hideNavItems);
   console.log("showNavItems function complete");
 }
 
@@ -27,21 +22,4 @@ function hideNavItems() {
     navChildren[j].style.display = "none";
   }
   console.log("hideNavItems function complete");
-}
-
-async function canHideNav() {
-  mousePresence = false;
-  await sleep(500);
-  if (!mousePresence) hideNavItems();
-  console.log("canHideNav function complete");
-}
-
-function refreshShowNav() {
-  mousePresence = true;
-  console.log("refreshShowNav function complete");
-}
-
-//sleep function taken directly from https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
 }
